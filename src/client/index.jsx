367e4b85eea31ce5e741d2e30e026e78b665dbba
n4to4 <n4to4k@gmail.com>
2017-07-23 14:55:24 +0900

    router

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -6,6 +6,7 @@ import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './app'
 import helloReducer from './reducer/hello'
@@ -22,9 +23,11 @@ const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
 
 const wrapApp = (AppComponent, reduxStore) => (
   <Provider store={reduxStore}>
-    <AppContainer>
-      <AppComponent />
-    </AppContainer>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
   </Provider>
 )
 
