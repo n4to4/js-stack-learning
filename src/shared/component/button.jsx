26fd52ccf7b635956d8cca5eb322bba4cb451bdc
n4to4 <n4to4k@gmail.com>
2017-07-23 17:05:03 +0900

    bootstrap

--- src/shared/component/button.jsx
+++ src/shared/component/button.jsx
@@ -7,7 +7,14 @@ type Props = {
   handleClick: Function,
 }
 
-const Button = ({ label, handleClick }: Props) =>
-  <button onClick={handleClick}>{label}</button>
+const Button = ({ label, handleClick }: Props) => (
+  // eslint-disable-next-line jsx-a11y/no-redundant-roles
+  <button
+    onClick={handleClick}
+    className="btn btn-primary"
+    type="button"
+    role="button"
+  >{label}</button>
+)
 
 export default Button
---
+++ src/shared/component/modal-example.jsx
@@ -0,0 +1,26 @@
+// @flow
+
+import React from 'react'
+
+/* eslint-disable jsx-a11y/no-redundant-roles */
+const ModalExample = () => (
+  <div className="js-modal-example modal fade">
+    <div className="modal-dialog">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title">Modal title</h5>
+          <button type="button" className="close" data-dismiss="modal">×</button>
+        </div>
+        <div className="modal-body">
+          This is a Bootstrap modal. It uses jQuery.
+        </div>
+        <div className="modal-footer">
+          <button type="button" role="button" className="btn btn-primary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+/* eslint-enable jsx-a11y/no-redundant-roles */
+
+export default ModalExample
